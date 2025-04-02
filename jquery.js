bc023ad0cac14031730495700e94d0918faaf8c6
avsaushkin <saushkin.av@gmail.com>
2025-04-02 23:33:14 +0500

    поменял свайпер

--- jquery.js
+++ jquery.js
@@ -2,15 +2,18 @@ let swiperinit = false;
 let mobileswiper;
 
 function swiperOn() {
-    if (window.innerWidth <= 767 && !swiperinit) {
-        // Добавляем .swiper-wrapper, если его ещё нет
-        if (!$('.mslider').find('.swiper-wrapper').length) {
-            $('.mslider').wrapInner('<div class="swiper-wrapper"></div>');
+    const screenWidth = window.innerWidth;
+    const mslider = $('.mslider');
+
+    if (screenWidth <= 767 && !swiperinit) {
+        // Убедиться, что обертка swiper-wrapper добавляется только один раз
+        if (!mslider.find('.swiper-wrapper').length) {
+            mslider.wrapInner('<div class="swiper-wrapper"></div>');
         }
-        
-        // Добавляем пагинацию, если её ещё нет
-        if (!$('.mslider').find('.swiper-pagination').length) {
-            $('.mslider').append('<div class="swiper-pagination"></div>');
+
+        // Убедиться, что пагинация добавляется только один раз
+        if (!mslider.find('.swiper-pagination').length) {
+            mslider.append('<div class="swiper-pagination"></div>');
         }
 
         // Инициализируем Swiper
@@ -20,8 +23,8 @@ function swiperOn() {
                 el: '.swiper-pagination',
                 clickable: true,
             },
-            loop: true, // Бесконечный режим
-            spaceBetween: 10, // Отступ между слайдами
+            loop: true,
+            spaceBetween: 10,
             breakpoints: {
                 0: { slidesPerView: 1 },
                 576: { slidesPerView: 2 }
@@ -29,25 +32,39 @@ function swiperOn() {
         });
 
         swiperinit = true;
-    } else if (window.innerWidth > 767 && swiperinit) {
-        // Уничтожаем Swiper, если он был создан
+    } else if (screenWidth > 767 && swiperinit) {
+        // Уничтожаем Swiper
         if (mobileswiper) {
             mobileswiper.destroy(true, true);
+            mobileswiper = null;
         }
-        swiperinit = false;
 
-        // Корректно убираем обертку swiper-wrapper и пагинацию
-        $('.mslider .swiper-wrapper').replaceWith($('.mslider .swiper-wrapper').html());
-        $('.mslider .swiper-pagination').remove();
+        // Восстановление HTML к изначальному виду, удаляя обертку и пагинацию
+        mslider.find('.swiper-wrapper').contents().unwrap();  // берет содержимое .swiper-wrapper и удаляет саму обертку
+        mslider.find('.swiper-pagination').remove();
+
+        swiperinit = false;
     }
 }
 
 // Запускаем функцию при загрузке страницы
-swiperOn();
-
-// Оптимизированный обработчик resize
-let resizeTimer;
-$(window).resize(() => {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(swiperOn, 200);
+$(document).ready(function(){
+    swiperOn();
+    $(window).resize(debounce(swiperOn, 200));
 });
+
+// Функция debounce, чтобы минимизировать количество вызовов функции resize
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function() {
+        const context = this, args = arguments;
+        const later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
